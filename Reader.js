dc9ba2723a9942e229c6a5c59fa97da82a81acc7
arthurpiratas <silva.arthursoares@gmail.com>
2022-03-10 22:18:03 -0300

    Criando classe Reader

---
+++ Reader.js
@@ -0,0 +1,19 @@
+const fs = require("fs")
+
+class Reader{
+
+    Read(filepath){
+
+        fs.readFile(filepath, "utf8", (err, data) => {
+            if(err){
+                console.log(err)
+            }else{
+                console.log(data)
+            }
+        })
+    }
+
+}
+
+
+module.exports = Reader
---
+++ app.js
@@ -0,0 +1,5 @@
+const Reader = require("./Reader")
+
+var leitor = new Reader()
+
+leitor.Read("./usuarios.csv")
